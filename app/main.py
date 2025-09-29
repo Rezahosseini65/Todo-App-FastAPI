6cc279b6cd1206a9568c11d97d7c6616c09d2861
RezaHosseini65 <hosseini.reza65@gmail.com>
2025-09-29 10:51:25 +0330

    initial project setup

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from routers import router as app_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application Startup")
+    yield
+    print("Application Shutdown")
+
+app = FastAPI(
+    lifespan=lifespan,
+    title="Simple Todo App Api",
+    description="this is a simple blog app with minimal usage of authentication and post managing",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Reza Hosseini",
+        "email": "hosseini.reza65@gmail.com",
+    },
+    license_info={"name": "MIT"},
+    docs_url="/swagger",
+    )
+
+app.include_router(app_router)
